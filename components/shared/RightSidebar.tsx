6d33339629f148376a630d8f83fba895005d21c0
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-12-03 16:47:27 +0700

    scroll able

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,13 +1,80 @@
-function RightSidebar() {
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import UserCard from "../cards/UserCard";
+import CommunityCard from "../cards/CommunityCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+
+async function RightSidebar() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  // Fetch users
+  const userResult = await fetchUsers({
+    userId: user.id,
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  // Fetch communities
+  const communityResult = await fetchCommunities({
+    searchString: "",
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
   return (
     <section className="custom-scrollbar rightsidebar">
       <div className="flex flex-1 flex-col justify-start">
         <h3 className="text-heading4-medium text-light-1">
           Suggested Communities
         </h3>
+        <div className="mt-14 flex flex-col gap-9 overflow-auto">
+          {communityResult.communities.length === 0 ? (
+            <p className="no-result">No users</p>
+          ) : (
+            <>
+              {communityResult.communities.map((community: any) => (
+                <CommunityCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  bio={community.bio}
+                  members={community.members}
+                />
+              ))}
+            </>
+          )}
+        </div>
       </div>
       <div className="flex flex-1 flex-col justify-start">
         <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+        <div className="mt-14 flex flex-col gap-9 overflow-auto">
+          {userResult.users.length === 0 ? (
+            <p className="no-result">No users</p>
+          ) : (
+            <>
+              {userResult.users.map((person: any) => (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType="User"
+                />
+              ))}
+            </>
+          )}
+        </div>
       </div>
     </section>
   );
